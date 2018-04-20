a3a67fa0abbea23d6e8193daf3eb089fff779e41
gabsc89o <user138066@eu249.local>
2018-04-20 16:58:40 +0100

    Initial Commit

---
+++ SQliteOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQliteOS
+//
+//  Created by user138066 on 4/20/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQliteOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQliteOS
+//
+//  Created by user138066 on 4/20/18.
+//  Copyright © 2018 user138066. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
